7094bbbfc47f04629a904414fb7880e32272f163
Alex Braddy <abraddy24@users.noreply.github.com>
2016-02-07 21:05:44 -0500

    polished file
    cleaned up file for readability

--- assignment3sub.ml
+++ assignment3sub.ml
@@ -51,8 +51,17 @@ type play = shape list
    returns the result of that check.
    Type: check -> result
 *)
-let result ((chk) : check) : result = match chk with | (Paper, Rock) | (Scissors, Paper) | (Rock, Scissors) -> FstWin | (Rock, Paper) | (Paper, Scissors) | (Scissors, Rock) -> SndWin | (Rock, Rock) | (Paper, Paper) | (Scissors, Scissors) -> Tie
-
+let result ((chk) : check) : result = 
+   match chk with 
+      | (Paper, Rock) 
+      | (Scissors, Paper) 
+      | (Rock, Scissors) -> FstWin 
+      | (Rock, Paper) 
+      | (Paper, Scissors) 
+      | (Scissors, Rock) -> SndWin 
+      | (Rock, Rock) 
+      | (Paper, Paper) 
+      | (Scissors, Scissors) -> Tie
 
 
 (*
@@ -60,9 +69,9 @@ let result ((chk) : check) : result = match chk with | (Paper, Rock) | (Scissors
    whether the check's result is a tie.
    Type: check -> bool
 *)
-let is_tie ((chk) : check) : bool = result chk = Tie
 
-
+let is_tie ((chk) : check) : bool = 
+   result chk = Tie
 
 
 (*
@@ -72,8 +81,12 @@ let is_tie ((chk) : check) : bool = result chk = Tie
    other, stop at the shortest one.
    Type: play * play -> game
 *)
-let rec game_from_plays ((ply1, ply2) : play * play) : game = match (ply1, ply2) with | ([], []) | ([], _) | (_, []) -> ([]) | (head1 :: rest1, head2 :: rest2) -> (head1, head2) :: game_from_plays (rest1, rest2)
-
+let rec game_from_plays ((ply1, ply2) : play * play) : game = 
+   match (ply1, ply2) with 
+      | ([], []) 
+      | ([], _) 
+      | (_, []) -> ([]) 
+      | (head1 :: rest1, head2 :: rest2) -> (head1, head2) :: game_from_plays (rest1, rest2)
 
 
 (*
@@ -81,16 +94,27 @@ let rec game_from_plays ((ply1, ply2) : play * play) : game = match (ply1, ply2)
    a valid game as described above.
    Type: game -> bool
 *)
-let rec valid_game ((gme) : game) : bool = match gme with | [] -> false | head :: rest -> let ans =  is_tie head = true in let continue = valid_game rest in if ans = false || continue = false then false else ans = continue
-
+let rec valid_game ((gme) : game) : bool = 
+   match gme with 
+      | [] -> false 
+      | head :: [] -> is_tie head = false  
+      | head :: rest -> let ans = is_tie head = true 
+                        in let continue = valid_game rest 
+                        in if ans = false || continue = false 
+                           then false 
+                           else ans = continue
 
 
 (*
    Write a function `play_game` that plays the game as described above.
    Type: game -> result
 *)
-let rec play_game (gme : game) : result = match gme with | [] -> Tie | head :: rest -> if is_tie head = false then result head else play_game rest
-
+let rec play_game (gme : game) : result = 
+   match gme with 
+      | [] -> Tie 
+      | head :: rest -> if is_tie head = false 
+                        then result head 
+                        else play_game rest
 
 (* --------------------------------------
             TEMPERATURES
@@ -111,9 +135,10 @@ type temp = C of float | F of float
    them to distinguish from the integer ones. For example "2.1 +. 5.2"
    Type: temp -> float
 *)
-let to_f ((tmp) : temp) : float = match tmp with | F f -> f | C c -> 1.8 *. c +. 32.0
-
-
+let to_f ((tmp) : temp) : float = 
+   match tmp with 
+      | F f -> f 
+      | C c -> 1.8 *. c +. 32.0
 
 (*
    Write a function `temp_compare` that takes as input a pair of temperatures and
@@ -121,8 +146,26 @@ let to_f ((tmp) : temp) : float = match tmp with | F f -> f | C c -> 1.8 *. c +.
    equal and -1 if the second temperature is higher.
    Type: temp * temp -> int
 *)
-let temp_compare ((tmp1, tmp2) : temp * temp) : int = match (tmp1, tmp2) with | (C n1, C n2) | (F n1, F n2) -> if n1 = n2 then 0 else if n1 > n2 then 1 else -1 | (C n1, F n2) -> let n2' = to_f (C n1) in if n2 = n2' then 0 else if n2 > n2' then -1 else 1 | (F n1, C n2) -> let n1' = to_f (C n2) in if n1 > n1' then 0 else if n1 > n1' then 1 else -1
-
+let temp_compare ((tmp1, tmp2) : temp * temp) : int = 
+   match (tmp1, tmp2) with 
+      | (C n1, C n2) 
+      | (F n1, F n2) -> if n1 = n2 
+                        then 0 
+                        else if n1 > n2 
+                             then 1 
+                             else -1 
+      | (C n1, F n2) -> let n2' = to_f (C n1) 
+                        in if n2 = n2' 
+                        then 0 
+                        else if n2 > n2' 
+                             then -1 
+                             else 1 
+      | (F n1, C n2) -> let n1' = to_f (C n2) 
+                        in if n1 > n1' 
+                           then 0 
+                           else if n1 > n1' 
+                                then 1 
+                                else -1
 
 
 (*
@@ -133,8 +176,10 @@ let temp_compare ((tmp1, tmp2) : temp * temp) : int = match (tmp1, tmp2) with | 
    to strings.
    Type: temp -> string
 *)
-let string_of_temp ((tmp) : temp) : string = match tmp with | C n -> string_of_float n ^ "C" | F n -> string_of_float n ^ "F"
-
+let string_of_temp ((tmp) : temp) : string = 
+   match tmp with 
+      | C n -> string_of_float n ^ "C" 
+      | F n -> string_of_float n ^ "F"
 
 
 
@@ -144,8 +189,18 @@ let string_of_temp ((tmp) : temp) : string = match tmp with | C n -> string_of_f
    if the list is empty.
    Type: temp list -> temp
 *)
-let rec max_temp ((tmplst) : temp list) : temp = match tmplst with | [] -> raise (Failure "max_temp") | head :: rest -> if rest = [] then head else let mx = max_temp rest in let comp = temp_compare (head, mx) in if comp = 1 then head else if comp = -1 then max_temp rest else head
-
+let rec max_temp ((tmplst) : temp list) : temp = 
+   match tmplst with 
+      | [] -> raise (Failure "max_temp") 
+      | head :: rest -> if rest = [] 
+                        then head 
+                        else let mx = max_temp rest 
+                             in let comp = temp_compare (head, mx) 
+                                in if comp = 1 
+                                   then head 
+                                   else if comp = -1 
+                                        then max_temp rest 
+                                        else head
 
 
 (*
@@ -153,5 +208,17 @@ let rec max_temp ((tmplst) : temp list) : temp = match tmplst with | [] -> raise
    recursive calls are tail calls. You will likely need to define an auxiliary
    function and use state recursion.
 *)
-let rec max_temp2 ((tmplst) : temp list) : temp = if tmplst = [] then raise (Failure "max_temp2") else let rec aux (lst) : temp = match lst with | [] -> F (1000000.0) | head :: rest -> let comp = temp_compare (head, aux rest) in if comp = 1 then head else if comp = -1 then aux rest else head in aux tmplst
+let rec max_temp2 ((tmplst) : temp list) : temp = 
+   if tmplst = [] 
+   then raise (Failure "max_temp2") 
+   else let rec aux (lst) : temp = 
+                  match lst with 
+                     | [] -> F (0.0) 
+                     | head :: rest -> let comp = temp_compare (head, aux rest) 
+                                       in if comp = 1 
+                                          then head 
+                                          else if comp = -1 
+                                          then aux rest 
+                                          else head 
+        in aux tmplst
 
