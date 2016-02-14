eb441b4c74f187e1aff1c7fe3e371079c3d4282e
Alex Braddy <abraddy24@users.noreply.github.com>
2016-02-14 17:26:18 -0500

    Create assignment5tests.ml

---
+++ assignment5tests.ml
@@ -0,0 +1,30 @@
+(* CALCULATIONS *)
+let t8a = has_vars (Add (Var, Int 2))
+let t8b = not (has_vars (Add (Int 1, Int 2)))
+
+let t9a = count_vars (Add (Var, Int 2)) = 1
+let t9b = count_vars (Add (Int 1, Int 2)) = 0
+
+let t10a = calc_eval (Add (Var, Int 2), 3) = 5
+
+let t11a = func_of_calc (Add (Var, Int 2)) 3 = 5
+
+let t12a = subst (Add (Var, Int 1), Mul (Var, Var)) =
+                Mul (Add (Var, Int 1), Add (Var, Int 1))
+
+let t13a = power 3 = Mul (Mul (Var, Var), Var)
+
+let t14a = term (2, 1) = Mul(Int 2, Var)
+
+let t15a = poly [(2, 1); (1, 4)] = Add (term (2, 1), term (1, 4))
+let t15b = poly [(2, 1); (0, 2); (1, 4)] = Add (term (2, 1), term (1, 4))
+let t15c = poly [(0, 1); (0, 4)] = Int 0
+let t15d = poly [(0, 1)] = Int 0
+let t15e = poly [] = Int 0
+let t15f = poly [(2, 1); (0, 1)] = term (2, 1)
+let t15g = poly [(0, 1); (2, 1)] = term (2, 1)
+
+let t16a = simplify (Add (Int 0, Var)) = Var
+let t16b = simplify (Add (Int 3, Int 4)) = Int 7
+let t16c = calc_eval (simplify (poly [(2, 1); (1, 0)]), 3) = 7
+
