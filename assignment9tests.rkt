1448a57f9e79b81dab8bde1e67b49d1191f583eb
Alex Braddy <abraddy24@users.noreply.github.com>
2016-03-17 21:09:34 -0400

    implemented tests
    wrote tests and all functions pass

--- assignment9tests.rkt
+++ assignment9tests.rkt
@@ -1,40 +1,47 @@
-#lang racket
-;; Programming Languages, Assignment 9 tests
-;;
-;; This file uses a basic testing mechanism similar to what we did with OCAML.
-;; To test, simply run this file in DrRacket.
+(equal? (add-nums (list)) 0) 
+(equal? (add-nums (list 4 2 'a 4)) 10) 
 
-(require "assignment9sub.rkt")
 
-;; add-nums
-(equal? (add-nums (list)) 0) ;;empty list
-(equal? (add-nums (list 1 2 'a 3)) 6) ;; non-number
+(equal? (length (list)) 0) 
+(equal? (length (list 4 8)) 2) 
 
-;; length
-(equal? (length (list)) 0) ;; empty list
 
-;; get-nth
 (with-handlers ([exn:fail? (lambda (exn) (equal? (exn-message exn)
                                                  "negative index"))])
-    (get-nth null -2))   ;;negative index
+    (get-nth null -2))  
+(equal? (get-nth (list 1 2 3) 2) 3)
 
-;; every-other
-(equal? (every-other (list 1 2 3 4)) (list 1 3)) ;; even length
-(equal? (every-other (list 1 2 3)) (list 1 3))   ;; odd length
+(equal? (every-other (list 7 2 6 8 5)) (list 7 6 5)) 
+(equal? (every-other (list 1 3)) (list 1))   
+(equal? (every-other (list)) (list)) 
 
-;; map
-(equal? (map (lambda (x) (* x x)) (list 1 2 3))
-     (list 1 4 9))       ;; squaring
 
-;; map2
-(equal? (map2 (lambda (x y) (* x y)) (list 1 2 3) (list 2 3 4))
-     (list 2 6 12))      ;; multiply
+(equal? (map (lambda (x) (* x x)) (list))
+     (list))      
+(equal? (map (lambda (x) (+ x x)) (list 1 2 3))
+     (list 2 4 6))       
 
-;; filter
+
+(equal? (map2 (lambda (x y) (* x y)) (list 1 3 5) (list 2 4 6))
+     (list 2 12 30))      
+(equal? (map2 (lambda (x y) (* x y)) (list 1) (list 2 8 9))
+     (list 2))      
+(equal? (map2 (lambda (x y) (* x y)) (list) (list 1 4 7))
+     (list))      
+
+
 (equal? (filter (lambda (x) (= (modulo x 2) 1))
-               (list 1 2 3 4))
-     (list 1 3))      ;; odd
+               (list 1 2 3 4 5 6 7 8))
+     (list 1 3 5 7))      
+(equal? (filter (lambda (x) (= (modulo x 2) 0))
+               (list 1 2 3 4 5 6 7 8))
+     (list 2 4 6 8))  
+(equal? (filter (lambda (x) (= (modulo x 2) 1))
+               (list))
+     (list))      
 
-;; call-all
-(equal? (call-all (list (lambda () 2)))
-     (list 2))        ;; one-element
+
+(equal? (call-all (list (lambda () 4) (lambda () 6) (lambda () 8)))
+     (list 4 6 8))        
+(equal? (call-all (list))
+     (list))   
